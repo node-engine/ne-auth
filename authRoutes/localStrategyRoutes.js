16bfe4f0bf46b7490f26cbdfc8fb8939f74b4f92
bernardhamann <info@node-engine.com>
2015-10-09 00:03:43 +0200

    =>

--- authRoutes/localStrategyRoutes.js
+++ authRoutes/localStrategyRoutes.js
@@ -53,6 +53,70 @@ var neLocalStrategyRoutes = function (server, passport){
         })(req, res, next);
     });
 
+    router.post('/change-password', function(req,res, next){
+
+        passport.authenticate('localStrategyChangePassword', function(err, user, info){
+
+            if (err !== null) {
+                var redirectPath = '/profile' + '?message=Error'
+                return res.redirect(redirectPath);
+            }
+            else if (info.error) {
+                var redirectPath = '/profile' + '?message='+ info.error;
+                return res.redirect(redirectPath);
+            }
+            else if (info.token) {
+                return res.cookie('token' ,info.token, { httpOnly: true, expire : new Date() + info.expire })
+                    .redirect('/profile');
+            }
+            else{
+                var redirectPath = '/profile' + '?message=UnknownError';
+                return res.redirect(redirectPath);
+            }
+
+        })(req, res, next);
+    });
+
+    router.post('/change-email', function(req,res, next){
+
+        passport.authenticate('localStrategyChangeEmail', function(err, user, info){
+
+            if (err !== null) {
+                var redirectPath = '/profile' + '?message=Error'
+                return res.redirect(redirectPath);
+            }
+            else if (info.error) {
+                var redirectPath = '/profile' + '?message='+ info.error;
+                return res.redirect(redirectPath);
+            }
+            else if (info.token) {
+                return res.cookie('token' ,info.token, { httpOnly: true, expire : new Date() + info.expire })
+                    .redirect('/profile');
+            }
+            else{
+                var redirectPath = '/profile' + '?message=UnknownError';
+                return res.redirect(redirectPath);
+            }
+
+        })(req, res, next);
+    });
+
+
+
+
+
+    router.post('/change-password', function(req, res, next){
+
+        if(req.claims && req.claims.user){
+            axios({
+                method: 'put',
+                url: '/api/users/' + req.claims.user +"?token=" + meta.token + "&'local.password'=" + req.query.password
+            });
+        }
+
+
+    });
+
 
     /*
      router.post('/link',
