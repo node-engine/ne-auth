779af13a01fb4e7e0971d665d98da969cc569133
bernardhamann <info@node-engine.com>
2015-10-08 17:15:40 +0200

    fixed the profile message

---
+++ handlers/neAuthProfileHandler.js
@@ -0,0 +1,81 @@
+var React = require('react');
+
+var meta = {
+    path: "/profile",
+    title: "Profile Page",
+    description: "User profile page",
+    neDataBefore: 1,
+    nedb1: {
+        pathFunction: function (meta) {
+            path = process.env.ROOTURL + "/api/users" + "/" + meta.claims.user +"?token="+ meta.token;
+            return path
+        },
+        cycle: false,
+        search: false
+    },
+};
+
+var handler = React.createClass({
+
+    render: function() {
+        var self = this;
+
+        console.log(self.props);
+        console.log(self.props.data.nedb1);
+
+        var facebook;
+        if (self.props.data && self.props.data.nedb1[0]){
+            if(self.props.data.nedb1[0].facebook.active === false){
+
+                facebook = (
+                    <div>
+                        <h3>Facebook</h3>
+                        <p>You are not connected to facebook</p>
+                    </div>
+                )
+            }
+            else {
+                var facebookId = self.props.data.nedb1[0].facebook.id;
+                var facebookToken = self.props.data.nedb1[0].facebook.token;
+                facebook = (
+                    <div>
+                        <h3>Facebook</h3>
+
+                        <p>Id: <br/>{facebookId}</p>
+
+                        <p>Token: <br/>{facebookToken}</p>
+                    </div>
+                )
+            }
+        }
+
+        var profile;
+        if (self.props.meta.claims && self.props.meta.claims.displayName){
+            var name = self.props.meta.claims.displayName;
+            profile = (
+                <div>
+                    <p>Welcome {name} </p>
+                    {facebook}
+                </div>
+            )
+        }
+        else {
+            profile = (
+                <div>
+                    <p>Please login to view your profile page</p>
+                </div>
+            )
+        }
+        return (
+            <body>
+            <h2 id="main-title">This is the Profile Handler</h2>
+
+            {profile}
+
+            </body>
+        )
+    }
+});
+
+exports.handler = handler;
+exports.meta = meta;
